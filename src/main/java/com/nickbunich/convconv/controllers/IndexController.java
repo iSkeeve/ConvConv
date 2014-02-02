3da3e91538753b7d0e80d2b2403163379d72fbdc
Nick Bunich <nickbunich@gmail.com>
2014-02-03 02:24:49 +0400

    setting up simple web context

---
+++ src/main/java/com/nickbunich/convconv/controllers/IndexController.java
@@ -0,0 +1,23 @@
+package com.nickbunich.convconv.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * User: Nick Bunich
+ * Date: 2/3/14
+ * Time: 12:56 AM
+ */
+
+@Controller
+@RequestMapping(value = "/")
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("message", "It's working!");
+        return "index";
+    }
+}
